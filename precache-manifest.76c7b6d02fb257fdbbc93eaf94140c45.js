6b304060f0835d56a0cbd506bd03c27bd5361a47
Jan-Fleming Nickelsen <j.nickelsen@hmmh.de>
2020-08-19 16:14:26 +0200

    Updates

---
+++ precache-manifest.76c7b6d02fb257fdbbc93eaf94140c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a6f8f959b3dd4503152a4de5ccd67f",
+    "url": "/Fragebogen/index.html"
+  },
+  {
+    "revision": "2caf8a4e3a9d74a378f4",
+    "url": "/Fragebogen/static/css/main.da5526d2.chunk.css"
+  },
+  {
+    "revision": "91ef430e9a08b729c38d",
+    "url": "/Fragebogen/static/js/2.c0cdc67a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fragebogen/static/js/2.c0cdc67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2caf8a4e3a9d74a378f4",
+    "url": "/Fragebogen/static/js/main.0172d8df.chunk.js"
+  },
+  {
+    "revision": "bdc2fba179c2a2cec317",
+    "url": "/Fragebogen/static/js/runtime-main.f854fb0e.js"
+  }
+]);
