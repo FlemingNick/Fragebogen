ed419759c0d4e42e61073dbb0ea550c573670218
Jan-Fleming Nickelsen <j.nickelsen@hmmh.de>
2020-08-20 12:37:38 +0200

    added Iphone Layer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Fragebogen from './Fragebogen/Fragebogen'
+import Phone from './Fragebogen/phone.png'
 
 function App() {
   return (
     <div className="App">
-        <h1>Fragebogen</h1>
-        <Fragebogen/>
+        <div className="FragebogenContainer">
+            <img src={Phone} className="Phone"/>
+            <Fragebogen className="Fragebogen"/>
+        </div>
+
+
     </div>
   );
 }
